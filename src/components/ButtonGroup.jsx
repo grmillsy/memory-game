9333656543534c98e470d45fddbcca2e2d9bc2a3
Georgia Mills <georgiamills@Georgias-MacBook-Pro.local>
2024-01-06 21:15:40 +0000

    refactored the button group

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,41 +1,29 @@
 import {useState} from "react";
 
-const MODE = {
-    'EASY': 20,
-    'MEDIUM': 100,
-    'HARD': 200,
-    'BEAST': 1000
-}
-
+const MODE = [
+    {'Easy': 20},
+    {'Medium': 100},
+    {'Hard': 200},
+    {'BEAST': 1000}
+]
 export default function ButtonGroup({handleBtnClick}) {
     const [selectedBtn, setSelectedBtn] = useState(MODE.EASY)
 
     const _handleBtnClick = (mode) => {
         setSelectedBtn(mode)
     }
-return(
-    <div className="inline-flex bg-white rounded-lg shadow-sm">
-        <button type="button"
-                onClick={() => _handleBtnClick(MODE.EASY)}
-                className={`${selectedBtn === MODE.EASY ? 'bg-blue-300 hover:bg-blue-300' : 'hover:bg-gray-50 bg-white'} py-3 px-4 inline-flex items-center gap-x-2 -ms-px first:rounded-s-lg first:ms-0 last:rounded-e-lg text-sm font-medium focus:z-10 border border-gray-200 text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none`} >
-            Easy
-        </button>
-        <button type="button"
-                onClick={() => _handleBtnClick(MODE.MEDIUM)}
-                className={`${selectedBtn === MODE.MEDIUM ? 'bg-blue-300 hover:bg-blue-300' : 'hover:bg-gray-50 bg-white'} py-3 px-4 inline-flex items-center gap-x-2 -ms-px first:rounded-s-lg first:ms-0 last:rounded-e-lg text-sm font-medium focus:z-10 border border-gray-200 text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none`} >
-            Medium
-        </button>
-        <button type="button"
-                onClick={() => _handleBtnClick(MODE.HARD)}
-                className={`${selectedBtn === MODE.HARD ? 'bg-blue-300 hover:bg-blue-300' : 'hover:bg-gray-50 bg-white'} py-3 px-4 inline-flex items-center gap-x-2 -ms-px first:rounded-s-lg first:ms-0 last:rounded-e-lg text-sm font-medium focus:z-10 border border-gray-200 text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none`} >
-            Hard
-        </button>
-        <button type="button"
-                onClick={() => _handleBtnClick(MODE.BEAST)}
-                className={`${selectedBtn === MODE.BEAST ? 'bg-blue-300 hover:bg-blue-300' : 'hover:bg-gray-50 bg-white'} py-3 px-4 inline-flex items-center gap-x-2 -ms-px first:rounded-s-lg first:ms-0 last:rounded-e-lg text-sm font-medium focus:z-10 border border-gray-200 text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none`} >
-            BEAST
-        </button>
-    </div>
 
+    return(
+        <div className="inline-flex bg-white rounded-lg shadow-sm">
+            {MODE.map((m, i,) => {
+                return (
+                    <button type="button"
+                            onClick={() => _handleBtnClick(m)}
+                            className={`${selectedBtn === m? 'bg-blue-300 hover:bg-blue-300' : 'hover:bg-gray-50 bg-white'} py-3 px-4 inline-flex items-center gap-x-2 -ms-px first:rounded-s-lg first:ms-0 last:rounded-e-lg text-sm font-medium focus:z-10 border border-gray-200 text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none`} >
+                        {Object.keys(m)}
+                    </button>
+                )
+            })}
+        </div>
     )
 }
